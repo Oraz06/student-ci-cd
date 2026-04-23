58afd0d70cc5a374947e8a1044189b6c332c5a58
Oraz06 <tleulesov06@gmail.com>
2026-04-23 15:34:36 +0500

    Add jup function and tests fix

--- app.py
+++ app.py
@@ -1,39 +1,10 @@
 import math
 
-def add(a, b):
-    return a + b
 
-def subtract(a, b):
-    return a - b
+def jup(a, b):
+    return (a + b) % 2 == 0
 
-def multiply(a, b):
-    return a * b
 
-def divide(a, b):
-    if b == 0:
-        raise ValueError("0-ге бөлуге болмайды")
-    return a / b
-
-def power(a, b):
-    return a ** b
-
-def max_num(a, b):
-    return max(a, b)
-
-def is_even(a):
-    return a % 2 == 0
-
-def sqrt_num(a):
-    if a < 0:
-        raise ValueError("Теріс санның түбірі болмайды")
-    return math.sqrt(a)
 
 if __name__ == "__main__":
-    print("Қосу:", add(2, 3))
-    print("Азайту:", subtract(5, 3))
-    print("Көбейту:", multiply(2, 3))
-    print("Бөлу:", divide(10, 2))
-    print("Дәреже:", power(2, 3))
-    print("Максимум:", max_num(5, 3))
-    print("Жұп па:", is_even(4))
-    print("Түбір:", sqrt_num(16))
+    print("::", jup(2, 2))
--- test_app.py
+++ test_app.py
@@ -1,26 +1,5 @@
 from app import *
 
-def test_add():
-    assert add(2, 3) == 5
+def test_jup():
+    assert jup(2, 2) == 4
 
-def test_subtract():
-    assert subtract(5, 3) == 2
-
-def test_multiply():
-    assert multiply(2, 3) == 6
-
-def test_divide():
-    assert divide(10, 2) == 5
-
-def test_power():
-    assert power(2, 3) == 8
-
-def test_max_num():
-    assert max_num(5, 3) == 5
-
-def test_is_even():
-    assert is_even(4) == True
-    assert is_even(5) == False
-
-def test_sqrt():
-    assert sqrt_num(16) == 4.0
